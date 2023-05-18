e211f9f634a57dfd1124b0f0d3496b50174d1d12
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-05-17 20:34:17 -0400

    Getting lat and lon

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ let data = require('./data/weather.json');
 const cors = require('cors');
 
 //USE
+
 const app = express();
 app.use(cors());
 
@@ -21,50 +22,70 @@ const PORT = process.env.PORT || 3002;
 
 // ROUTES
 
-app.get('/', (request, response) => {
-  response.send('hello');
+
+// CLASS DEMO
+app.get('/weather', async (req, res, next) => {
+  // probably need a try catch
+  // the thing that the user is searching for:
+ 
+  let {lat, lon} = req.query.searchQuery;
+  // need the request URL from the API
+  // searchQuery -   if the user is seraching for kittens, pass that value into the URL I'm sending the API
+
+  let weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=5&lat=${lat}&lon=${lon}`;;
+  let photoData = await axios.get(url);
+
+  // console.log(photoData.data.results);
+  let picArray = photoData.data.results.map(pic => new Photo(pic));
+
+  res.status(200).send(picArray);
+
 });
-
-app.get('/sayHello', (request, response) => {
-  console.log('hi');
-  console.log(request.query.firstName);
-  let firstName = request.query.firstName;
-  let lastName = request.query.lastName;
-  response.send(`hi ${firstName} ${lastName}`);
-});
-
-app.get('/weather', (request, response, next) => {
-  // http://localhost:3001/weather?cityData=seattle
-  try {
-    // This just shows what city the user is searching for
-    let searchQuery = request.query.cityData;
-    // cityData is a searchQuery
-    let city = data.find(cityData => cityData.city_name.toLowerCase() === searchQuery.toLowerCase())
-
-    let dataMap = city.data.map(oneDay => new Forecast(oneDay));
-
-    response.status(200).send(dataMap);
-  } catch (error) {
-    next(error);
+// CLASS DEMO
+  
+  app.get('*', (request, response) => {
+    response.send('The thing you are looking for doesn\'t exist');
+  });
+  
+  //  CLASSES
+  
+  class Forecast {
+    constructor(day) {
+      this.date = day.valid_date;
+      this.description = day.weather.description;
+      this.high = day.high_temp;
+      this.low = day.low_temp;
+    }
   }
-});
-
-app.get('*', (request, response) => {
-  response.send('The thing you are looking for doesn\'t exist');
-});
-
-//  CLASSES
-
-class Forecast {
-  constructor(day) {
-    this.date = day.valid_date;
-    this.description = day.weather.description;
-    this.high = day.high_temp;
-    this.low = day.low_temp;
-  }
-}
+  
 
 
 // LISTEN 
+// NODEMON
 
+
+// app.get('/', (request, response) => {
+//   response.send('hello');
+// });
+
+// app.get('/weather', async (request, response, next) => {
+//   // http://localhost:3001/weather?cityData=seattle
+//   try {
+    // This just shows what city the user is searching for
+
+    // let {lat, lon} = request.query.searchQuery;
+    // let url - `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}`
+    // let weatherData = await axios.get(url);
+    
+//     let searchQuery = request.query.cityData;
+//     // cityData is a searchQuery
+//     let city = data.find(cityData => cityData.city_name.toLowerCase() === searchQuery.toLowerCase())
+
+//     let dataMap = city.data.map(oneDay => new Forecast(oneDay));
+
+//     response.status(200).send(dataMap);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
